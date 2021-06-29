03bb18d56ca5a808b6addea0e5bb49a065fe1446
krhmwn <krhmwn@users.noreply.github.com>
2021-06-29 14:24:26 +0700

    Update .eleventy.js  29.06.21

--- .eleventy.js
+++ .eleventy.js
@@ -136,3 +136,20 @@ module.exports = function(eleventyConfig) {
     }
   };
 };
+
+
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addLayoutAlias('default', 'base.njk');
+  eleventyConfig.addPassthroughCopy("site/css");
+  eleventyConfig.addPassthroughCopy("site/images");
+
+  return {
+    dir: {
+      input: "site",
+      layouts: "_layouts",
+      output: "dist"
+    }
+  };
+};
+
