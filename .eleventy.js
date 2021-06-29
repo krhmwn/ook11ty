128900767c6aa0d9d291c60d7865ddb07fa80f73
krhmwn <krhmwn@users.noreply.github.com>
2021-06-29 09:02:13 +0700

    update v.1.0.32

--- .eleventy.js
+++ .eleventy.js
@@ -27,6 +27,10 @@ module.exports = function(eleventyConfig) {
     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
   });
 
+  
+  eleventyConfig.addCollection("tagList", require("./_filters/getTagList.js "));
+  
+  
   // Get the first `n` elements of a collection.
   eleventyConfig.addFilter("head", (array, n) => {
     if( n < 0 ) {
